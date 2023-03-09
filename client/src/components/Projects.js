dfce5bcb613531936cd3fbe6f6266941aea98597
Leonnardo Nascimento <leonnardoenriquesn@gmail.com>
2023-03-09 14:39:03 -0800

    aadded projects

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -3,6 +3,7 @@ import react from "../media/react-svgrepo-com.svg";
 import mysql from "../media/mysql-svgrepo-com.svg";
 import nodejs from "../media/nodejs-svgrepo-com.svg";
 import python from "../media/python-svgrepo-com.svg";
+import typescript from "../media/typescript-svgrepo-com.svg";
 import ProjectCard from "./projects/ProjectCard";
 import myDriverApp from "../media/myDriverApp.svg";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -15,6 +16,18 @@ import mydriverappLogo from "../media/myDriverApp.svg";
 import mda2 from "../media/projects-images/mydriverapp/mediamodifier_image.png";
 import mda3 from "../media/projects-images/mydriverapp/mediamodifier_image (1).png";
 import mda1 from "../media/projects-images/mydriverapp/iphone202.png";
+
+import cisco1 from "../media/projects-images/cisco/cisco-1.jpg";
+import cisco2 from "../media/projects-images/cisco/cisco-2.jpg";
+import cisco3 from "../media/projects-images/cisco/cisco-3.jpg";
+
+import craig1 from "../media/projects-images/craig/craig-1.jpg";
+import craig2 from "../media/projects-images/craig/craig-2.jpg";
+import craig3 from "../media/projects-images/craig/craig-3.jpg";
+
+import cameraapp1 from "../media/projects-images/cameraapp/cameraapp-1.jpg";
+import cameraapp2 from "../media/projects-images/cameraapp/cameraapp-2.jpg";
+import cameraapp3 from "../media/projects-images/cameraapp/cameraapp-3.jpg";
 
 export default function Projects() {
   return (
@@ -45,10 +58,29 @@ export default function Projects() {
           desc={
             "The application enables users to order trips by utilizing the Google Maps API, which calculates prices based on distance and time. It has been developed using a combination of React, Express, Node.js, and MySQL technologies."
           }
-          title={"My Smart Camera "}
-          logo={mydriverappLogo}
-          technologies={[python]}
+          title={"Cisco | Software Engineering Experience Program"}
+          image1={cisco1}
+          image2={cisco2}
+          image3={cisco3}
+          logo={"https://www.svgrepo.com/show/473567/cisco.svg"}
+          technologies={[react, nodejs]}
           classes={"flex-row-reverse"}
+          projSc={"https://github.com/N-Leonnardo/cisco-starter-repo"}
+          projLive={""}
+        />
+        <ProjectCard
+          desc={
+            "Craigslist Jobs is a web application that collects job listings from Craigslist and presents them in an easy-to-use interface. It allows users to filter job listings based on location, job title, and keywords. Users can save job listings, share them with friends and receive daily email notifications about new job postings. However, users should be cautious of scams and verify the legitimacy of job listings before applying."
+          }
+          image1={craig1}
+          image2={craig2}
+          image3={craig3}
+          title={"Craigslist Jobs"}
+          logo={"https://www.svgrepo.com/show/473567/cisco.svg"}
+          technologies={[typescript, nodejs]}
+          classes={"flex-row-reverse"}
+          projSc={"https://github.com/N-Leonnardo/cisco-starter-repo"}
+          projLive={""}
         />
         <ProjectCard
           bg={mydriverappBg}
@@ -56,12 +88,13 @@ export default function Projects() {
           desc={
             "The application enables users to order trips by utilizing the Google Maps API, which calculates prices based on distance and time. It has been developed using a combination of React, Express, Node.js, and MySQL technologies."
           }
-          title={"Cisco | Software Engineering Experience Program"}
-          logo={"https://www.svgrepo.com/show/473567/cisco.svg"}
-          technologies={[react, nodejs]}
+          title={"My Smart Camera "}
+          image1={cameraapp1}
+          image2={cameraapp2}
+          image3={cameraapp3}
+          logo={mydriverappLogo}
+          technologies={[python]}
           classes={"flex-row-reverse"}
-          projSc={"https://github.com/N-Leonnardo/cisco-starter-repo"}
-          projLive={""}
         />
       </div>
     </div>
